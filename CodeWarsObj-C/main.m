6a9d287f6fc3e57e0e6a93900b20258220860699
Jinah Adam <jinahadam@gmail.com>
2019-02-09 12:27:08 +0800

    Initial Commit

---
+++ CodeWarsObj-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CodeWarsObj-C
+//
+//  Created by Jinah Adam on 9/2/19.
+//  Copyright © 2019 Jinah Adam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
